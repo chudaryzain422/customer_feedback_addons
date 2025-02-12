9fff671753a478dac50a26404fbab871c94d1490
Odoo Developer <zain@pitsolutions.com>
2025-02-12 21:53:25 +0400

    final template updated

--- pos_feedback/controllers/main.py
+++ pos_feedback/controllers/main.py
@@ -28,6 +28,7 @@ class SurveyController(Survey):
                 'points': 10,  # Assuming 10 points equals a 10% discount
                 'expiration_date': (datetime.today() + timedelta(days=5)).strftime('%Y-%m-%d'),
             })
+            partner.barcode = coupon.code
             self.send_whatsapp_message(partner, coupon)
             request.session.pop('feedback_order_id', None)
             request.session.pop('feedback_email', None)
@@ -35,74 +36,66 @@ class SurveyController(Survey):
             print(f"Created loyalty coupon for partner {partner.name}: {coupon.id}")
         return response, error
 
-    def send_whatsapp_message(self, partner, coupon):
-        # Implement your WhatsApp message-sending logic here.
-        try:
-            if partner.mobile:
-                provider = request.env['provider'].sudo().search(
-                    [('state', '=', 'enabled'), ('provider', '=', 'graph_api')], limit=1)
-                if provider and provider.graph_api_authenticated:
-                    url = provider.graph_api_url + provider.graph_api_instance_id + "/messages"
-                    message = f"Hello {partner.name}, thank you for your feedback! We have created a Discount coupon for you. Your coupon code is {coupon.code}."
-
-                    payload = json.dumps({
-                        "messaging_product": "whatsapp",
-                        "recipient_type": "individual",
-                        "to": partner.mobile,
-                        "type": "text",
-                        "text": {
-                            "body": message
-                        }
-                    })
-
-                    headers = {
-                        'Content-Type': 'application/json',
-                        'Authorization': 'Bearer ' + provider.graph_api_token
-                    }
-
-                    response = requests.post(url, headers=headers, data=payload)
-
-                    if response.status_code == 200:
-                        print(f"Sent WhatsApp message to {partner.mobile}")
-                    else:
-                        print(f"Failed to send WhatsApp message to {partner.mobile}: {response.text}")
-            else:
-                print(f"No phone number available for partner {partner.name}. WhatsApp message not sent.")
-        except Exception as e:
-            # Catch any error that occurs in the process
-            print(f"Error sending WhatsApp message to {partner.name} ({partner.id}): {str(e)}")
-
-
-
-
     # def send_whatsapp_message(self, partner, coupon):
     #     # Implement your WhatsApp message-sending logic here.
     #     try:
     #         if partner.mobile:
-    #             provider = request.env['provider'].sudo().search([('state', '=', 'enabled'),('provider', '=', 'graph_api')], limit=1)
+    #             provider = request.env['provider'].sudo().search(
+    #                 [('state', '=', 'enabled'), ('provider', '=', 'graph_api')], limit=1)
     #             if provider and provider.graph_api_authenticated:
-    #                 template = request.env['wa.template'].sudo().search([('state', '=', 'added'),('provider_id', '=', provider.id),('name', '=', 'new')], limit=1)
-    #                 if len(template.components_ids):
-    #                     for line in template.components_ids:
-    #                         if line.type == 'body':
-    #                             line.text = f"Hello {partner.name}, thank you for your feedback! We have created a Discount coupon for you. Your coupon code is {coupon.code}."
-    #                 # Use the WhatsApp API to send the message
-    #                 compose = request.env['wa.compose.message'].sudo().with_context(active_model="res.partner", active_id=partner.id).create({
-    #                     'partner_id':partner.id,
-    #                     'provider_id':provider.id,
-    #                     'template_id':template.id,
-    #                     'body': f"Hello {partner.name}, thank you for your feedback! We have created a Discount coupon for you. Your coupon code is {coupon.code}."
+    #                 url = provider.graph_api_url + provider.graph_api_instance_id + "/messages"
+    #                 message = f"Hello {partner.name}, thank you for your feedback! We have created a Discount coupon for you. Your coupon code is {coupon.code}."
+    #
+    #                 payload = json.dumps({
+    #                     "messaging_product": "whatsapp",
+    #                     "recipient_type": "individual",
+    #                     "to": partner.mobile,
+    #                     "type": "text",
+    #                     "text": {
+    #                         "body": message
+    #                     }
     #                 })
-    #                 if compose:
-    #                     compose.send_whatsapp_message()
-    #                 # Example:
-    #                 # whatsapp_api.send_message(partner.phone, message)
-    #                 print(f"Sent WhatsApp message to {partner.mobile}")
+    #
+    #                 headers = {
+    #                     'Content-Type': 'application/json',
+    #                     'Authorization': 'Bearer ' + provider.graph_api_token
+    #                 }
+    #
+    #                 response = requests.post(url, headers=headers, data=payload)
+    #
+    #                 if response.status_code == 200:
+    #                     print(f"Sent WhatsApp message to {partner.mobile}")
+    #                 else:
+    #                     print(f"Failed to send WhatsApp message to {partner.mobile}: {response.text}")
     #         else:
     #             print(f"No phone number available for partner {partner.name}. WhatsApp message not sent.")
     #     except Exception as e:
     #         # Catch any error that occurs in the process
     #         print(f"Error sending WhatsApp message to {partner.name} ({partner.id}): {str(e)}")
+
+
+
+
+    def send_whatsapp_message(self, partner, coupon):
+        # Implement your WhatsApp message-sending logic here.
+        try:
+            if partner.mobile:
+                provider = request.env['provider'].sudo().search([('state', '=', 'enabled'), ('provider', '=', 'graph_api')], limit=1)
+                if provider and provider.graph_api_authenticated:
+                    template = request.env['wa.template'].sudo().search([('state', '=', 'added'),('provider_id', '=', provider.id),('name', '=', 'ambi')], limit=1)
+                    compose = request.env['wa.compose.message'].sudo().with_context(active_model="res.partner", active_id=partner.id).create({
+                        'partner_id':partner.id,
+                        'provider_id':provider.id,
+                        'template_id':template.id,
+                    })
+                    if compose:
+                        compose.send_whatsapp_message()
+                    print(f"Sent WhatsApp message to {partner.mobile}")
+            else:
+                print(f"No phone number available for partner {partner.name}. WhatsApp message not sent.")
+        except Exception as e:
+            # Catch any error that occurs in the process
+            print(f"Error sending WhatsApp message to {partner.name} ({partner.id}): {str(e)}")
 
 class POSFeedbackController(http.Controller):
 
